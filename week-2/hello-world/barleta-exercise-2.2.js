d410233b03fb3e3cc44d5f2dc61c5e70d0bc3ed1
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-08-17 19:53:01 -0500

    commit

---
+++ week-2/hello-world/barleta-exercise-2.2.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title:  barleta-exercise-2.2.js
+; Author: Professor Krasso
+; Date:   17 August 2020
+; Modified by: Marie Nicole Barleta
+; Description: Hello World With Express
+;===========================================
+*/
+
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.use(function(req, res)
+{
+  console.log('In comes a request to: %s', req.url)
+  res.end('Hello World\n')
+})
+
+http.createServer(app).listen(8080, function()
+{
+  console.log('Application started on port %s', 8080);
+});
